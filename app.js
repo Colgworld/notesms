b3567234bb81d1a816c19a5fa8a7e705d62da90b
Colgworld <mkcolgan@gmail.com>
2019-11-13 20:41:05 -0500

    push

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { Strategy } = require('passport-local');
 const flash = require('flash');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
-const { Pool, Client } = require('pg')
+// const { Pool, Client } = require('pg')
 
 // Routes
 const usersRouter = require('./routes/users');
@@ -20,26 +20,26 @@ const verifyRouter = require('./routes/verify');
 const registerRouter = require('./routes/register');
 const notesRouter = require('./routes/notes');
 const analyzeRouter = require('./routes/analyze');
-const connectionString = process.env.URI
+// const connectionString = process.env.URI
 
-const pool = new Pool({
-  connectionString: connectionString,
-})
-
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-
-const client = new Client({
-  connectionString: connectionString,
-})
-
-client.connect()
-// client.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   client.end()
+// const pool = new Pool({
+//   connectionString: connectionString,
 // })
+
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+
+// const client = new Client({
+//   connectionString: connectionString,
+// })
+
+// client.connect()
+// // client.query('SELECT NOW()', (err, res) => {
+// //   console.log(err, res)
+// //   client.end()
+// // })
 
 const db = require('./models');
 const userRoles = require('./userRoles');
